5211db73294b1c68aaa119afe2838b7363f2a13b
Mikhail Kortikov <kortikov.m.r@gmail.com>
2025-04-08 16:01:12 +0300

    Equality Check Possibility

--- Bind.cs
+++ Bind.cs
@@ -7,18 +7,24 @@ namespace Bind
     public class Bind<T> : IBind<T>
     {
         public event Action<T> OnValueChange;
-        
+
+        private readonly bool _equalityCheck;
+        private readonly Func<T, T, bool> _equalityComparer;
         private T _value;
 
         public void AddListener(Action<T> listener) => OnValueChange += listener;
         public void RemoveListener(Action<T> listener) => OnValueChange -= listener;
         
-        public Bind()
+        public Bind(bool equalityCheck = false, Func<T, T, bool> equalityComparer = null)
         {
+            _equalityCheck = equalityCheck;
+            _equalityComparer = equalityComparer;
         }
 
-        public Bind(T value)
+        public Bind(T value, bool equalityCheck = false, Func<T, T, bool> equalityComparer = null)
         {
+            _equalityCheck = equalityCheck;
+            _equalityComparer = equalityComparer;
             _value = value;
         }
 
@@ -27,15 +33,29 @@ namespace Bind
             get => _value;
             set
             {
+                var isEqual = Equal(value);
                 _value = value;
-                OnValueChange?.Invoke(value);
+                if (!isEqual)
+                    OnValueChange?.Invoke(value);
             }
         }
 
+        public bool Equal(T value)
+        {
+            if (!_equalityCheck)
+                return false;
+            
+            if (_value == null && value != null || _value != null && value == null)
+                return false;
+            if (_equalityComparer != null && _equalityComparer(_value, value))
+                return false;
+            if (_value != null && value != null && !_value.Equals(value))
+                return false;
+            return true;
+        }
+        
         public void SetValue(T value) => V = value;
         public T GetValue() => V;
-        
-        public static implicit operator T(Bind<T> bind) => bind.V;
     }
 
     public class AsyncBind<T> : IAsyncBind<T>
@@ -46,25 +66,45 @@ namespace Bind
         public void RemoveListener(Func<T, CancellationTokenSource, Task> listener) => OnValueChangeAsync -= listener;
         
         public T V { get; private set; }
+        
+        private readonly bool _equalityCheck;
+        private readonly Func<T, T, bool> _equalityComparer;
 
-        public AsyncBind()
+        public AsyncBind(bool equalityCheck = false, Func<T, T, bool> equalityComparer = null)
         {
+            _equalityCheck = equalityCheck;
+            _equalityComparer = equalityComparer;
         }
 
-        public AsyncBind(T value)
+        public AsyncBind(T value, bool equalityCheck = false, Func<T, T, bool> equalityComparer = null)
         {
+            _equalityCheck = equalityCheck;
+            _equalityComparer = equalityComparer;
             V = value;
         }
 
         public async Task SetValueAsync(T value, CancellationTokenSource cancellationTokenSource = null)
         {
             V = value;
-            if (OnValueChangeAsync == null)
-                return;
-            
-            await OnValueChangeAsync.Invoke(value, cancellationTokenSource);
+            var isEqual = Equal(value);
+            if (!isEqual && OnValueChangeAsync != null)
+                await OnValueChangeAsync.Invoke(value, cancellationTokenSource);
         }
 
+        public bool Equal(T value)
+        {
+            if (!_equalityCheck)
+                return false;
+            
+            if (V == null && value != null || V != null && value == null)
+                return false;
+            if (_equalityComparer != null && _equalityComparer(V, value))
+                return false;
+            if (V != null && value != null && !V.Equals(value))
+                return false;
+            return true;
+        }
+        
         public T GetValue()
         {
             return V;
